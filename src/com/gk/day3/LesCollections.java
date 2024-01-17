cbda335bab6797ddbeb742f8833af02f3a3fa323
moahmed amine mezghich <git@mezghich.com>
2024-01-17 16:09:59 +0100

    les collections

--- src/com/gk/day3/LesCollections.java
+++ src/com/gk/day3/LesCollections.java
@@ -1,12 +1,14 @@
 package com.gk.day3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class LesCollections {
 
 	public static void main(String[] args) {
 
-		//Les tableaux
+		//1)Les tableaux
+		/*
 		String[] names = { "amine", "fatima", "benoit" };  //déclaration + création + initialisation
 		
 		String tab[] = new String[3]; // déclaration création d'un tableau avec 3 cases + initialisation par des null
@@ -46,7 +48,39 @@ public class LesCollections {
 		System.out.println(Arrays.binarySearch(t, 21));
 		
 		int t2[] = {32,14,220,30};
-		System.out.println(Arrays.binarySearch(t2, 30));
+		System.out.println(Arrays.binarySearch(t2, 30));*/
+		
+		//2)Les collections
+		
+		ArrayList<String> students  = new ArrayList<>(); // tableau dynamique
+		System.out.println(students.size()); // size = nombre d'elements
+		System.out.println(students.isEmpty()); // boolean
+		
+		students.add("Amine");  // add : permet d'ajouter des elements
+		students.add("Pierre");
+		students.add("Benoit");
+		System.out.println(students);
+		students.remove("Amine");
+		System.out.println(students);
+		students.add(0, "Ismail");
+		System.out.println(students);
+		students.remove(1);
+		System.out.println(students);
+		System.out.println(students.size());
+		System.out.println(students.isEmpty());
+		
+		
+		ArrayList<Integer>valeurs = new ArrayList<>();
+		valeurs.add(12);
+		valeurs.add(20);
+		valeurs.add(30);
+		System.out.println(valeurs);
+		/*
+		ArrayList  col  = new ArrayList();
+		col.add("amine");
+		col.add(12);
+		col.add(true);
+		System.out.println(col);*/
 	}
 
 }
