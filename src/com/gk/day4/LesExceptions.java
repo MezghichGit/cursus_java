777cb0920fdef0455c1a31f4e28e643cd008da63
moahmed amine mezghich <git@mezghich.com>
2024-01-18 12:05:37 +0100

    Correction version2

--- src/com/gk/day4/LesExceptions.java
+++ src/com/gk/day4/LesExceptions.java
@@ -3,28 +3,6 @@ package com.gk.day4;
 import java.util.Scanner;
 
 public class LesExceptions {
-	
-	public static void getData(Scanner sc) throws Exception
-	{
-		
-		System.out.println("Donner votre nom min 4 lettres");
-		String nom = sc.nextLine();
-		
-		if(nom.length()>4)
-			System.out.println("Nom valide : "+nom);
-		else
-			throw new Exception("Votre nom doit dépasser 4 caractères");
-		
-		System.out.println("Donner votre age : >0");
-		int age = sc.nextInt();
-		
-		if(age>0)
-			System.out.println("Age valide : "+age);
-		else
-			throw new Exception("Votre age doit être positif");
-		
-		
-	}
 
 	public static void division(int a, int b) throws ArithmeticException // peut déclencher une exception
 	{
@@ -36,27 +14,51 @@ public class LesExceptions {
 			System.out.println(a / b);
 	}
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		
+	public static void getData(Scanner sc) throws Exception {
+
+		if (sc != null) {
+
+			System.out.println("Donner votre nom min 4 lettres");
+			String nom = sc.nextLine();
+
+			if (nom.length() > 4)
+				System.out.println("Nom valide : " + nom);
+			else
+				throw new Exception("Votre nom doit dépasser 4 caractères");
+
+			System.out.println("Donner votre age : >0");
+			int age = sc.nextInt();
+
+			if (age > 0)
+				System.out.println("Age valide : " + age);
+			else
+				throw new Exception("Votre age doit être positif");
+		} else
+			throw new NullPointerException("Vérifier votre scanner!");
+
+	}
+
+	public static void main(String[] args) {// throws Exception {
+
+		 Scanner sc = new Scanner(System.in);
+
+		//Scanner sc = null;
 		try {
 			getData(sc);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Problème lors de la saisie de vos données : "+e.getMessage());
-		}
-		/*
-		System.out.println("Hello GK");
-		try {
-		   division(10,0);
-		}
-		catch(ArithmeticException e)
-		{
-			System.out.println(e.getMessage());
+		} 
+		catch (NullPointerException e) {
+			System.out.println("Problème :"+e.getMessage());
 		}
 		
-		System.out.println("Fin du programme");*/
+		catch (Exception e) {
+			System.out.println("Problème lors de la saisie de vos données : " + e.getMessage());
+		}
+		/*
+		 * System.out.println("Hello GK"); try { division(10,0); }
+		 * catch(ArithmeticException e) { System.out.println(e.getMessage()); }
+		 * 
+		 * System.out.println("Fin du programme");
+		 */
 		/*
 		 * int x = 10; int y = 5;
 		 */
@@ -70,11 +72,8 @@ public class LesExceptions {
 		 * e.printStackTrace(); }
 		 */
 		/*
-		String nom = null;
-		int x = 10;
-		int y = 0;
-		int tab[] = { 10, 2, 14 };
-		
+		 * String nom = null; int x = 10; int y = 0; int tab[] = { 10, 2, 14 };
+		 * 
 		 * try { System.out.println(nom.length());// System.out.println(x / y);//
 		 * System.out.println(tab[10]); } catch (NullPointerException e) {
 		 * System.out.println("Vérifier votre nom : "+e.getMessage());
@@ -89,4 +88,5 @@ public class LesExceptions {
 
 	}
 
+	
 }
